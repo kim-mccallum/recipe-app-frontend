31ddc933ba62f6a91fabc9dca560c9e51694f1ee
Kim McCallum <kchaase@gmail.com>
2020-12-25 08:15:48 -0700

    basic structure and routes setup for users and recipes

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -5,7 +5,7 @@ const Users = () => {
   const USERS = [
     {
       id: "u1",
-      name: "Boots's cousin",
+      name: "Boots' cousin, Socks",
       image:
         "https://images.unsplash.com/photo-1575510632337-8550e3993a9b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=401&q=80",
       places: 3,
