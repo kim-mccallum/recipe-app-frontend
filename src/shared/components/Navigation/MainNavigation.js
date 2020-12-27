9f7521bec86db9929631a8dfc9d0713faf260370
Kim McCallum <kchaase@gmail.com>
2020-12-26 17:59:19 -0700

    added sidedrawer

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,21 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+
 import MainHeader from "./MainHeader";
 import NavLinks from "./NavLinks";
 import SideDrawer from "./SideDrawer";
+import Backdrop from "../UIElements/Backdrop";
 import "./MainNavigation.css";
 
 const MainNavigation = (props) => {
-  // everything in between the opening/closing brackets of the child component gets passed down as props.children
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDrawerHandler = () => {
+    setDrawerIsOpen(true);
+  };
+  const closeDrawerHandler = () => {
+    setDrawerIsOpen(false);
+  };
+
+  // everything inbetween the opening/closing brackets of the child component gets passed down as props.children
   return (
     <React.Fragment>
-      <SideDrawer>
+      {/* Logical && means if the first thing is true then the next gets rendered */}
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
         <nav className="main-navigation__drawer-nav">
           <NavLinks />
         </nav>
       </SideDrawer>
+
       <MainHeader>
-        <button className="main-navigation__menu-btn">
+        <button
+          className="main-navigation__menu-btn"
+          onClick={openDrawerHandler}
+        >
           <span></span>
           <span></span>
           <span></span>
