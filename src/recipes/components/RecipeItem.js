b5d791d1531ddcf5f95acd39ae9f8d75d6070c58
Kim McCallum <kchaase@gmail.com>
2020-12-28 11:26:19 -0700

    added recipe modal

--- src/recipes/components/RecipeItem.js
+++ src/recipes/components/RecipeItem.js
@@ -1,27 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Card from "../../shared/components/UIElements/Card";
 import Button from "../../shared/components/FormElements/Button";
+import Modal from "../../shared/components/UIElements/Modal";
 import "./RecipeItem.css";
 
 const RecipeItem = (props) => {
+  const [showRecipe, setShowRecipe] = useState(false);
+  const openRecipeHandler = () => setShowRecipe(true);
+  const closeRecipeHandler = () => setShowRecipe(false);
   return (
-    <li className="recipe-item">
-      <Card className="recipe-item">
-        <div className="recipe-item__image">
-          <img src={props.image} alt={props.title} />
+    <React.Fragment>
+      <Modal
+        show={showRecipe}
+        onCancel={closeRecipeHandler}
+        header={props.title}
+        contentClass="recipe-item__modal-content"
+        footerClass="recipe-item__modal-actions"
+        footer={<Button onClick={closeRecipeHandler}>CLOSE</Button>}
+      >
+        <div className="recipe-container">
+          <h2>THE RECIPE!</h2>
         </div>
-        <div className="recipe-item__info">
-          <h2>{props.title}</h2>
-          <p>{props.description}</p>
-        </div>
-        <div className="recipe-item__actions">
-          <Button inverse>VIEW RECIPE</Button>
-          <Button to={`/places/${props.id}`}>EDIT</Button>
-          <Button danger>DELETE</Button>
-        </div>
-      </Card>
-    </li>
+      </Modal>
+      <li className="recipe-item">
+        <Card className="recipe-item">
+          <div className="recipe-item__image">
+            <img src={props.image} alt={props.title} />
+          </div>
+          <div className="recipe-item__info">
+            <h2>{props.title}</h2>
+            <p>{props.description}</p>
+          </div>
+          <div className="recipe-item__actions">
+            <Button inverse onClick={openRecipeHandler}>
+              VIEW RECIPE
+            </Button>
+            <Button to={`/places/${props.id}`}>EDIT</Button>
+            <Button danger>DELETE</Button>
+          </div>
+        </Card>
+      </li>
+    </React.Fragment>
   );
 };
 
