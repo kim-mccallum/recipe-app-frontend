32a157586094b236b5ecf247bc908c4c44147c27
Kim McCallum <kchaase@gmail.com>
2020-12-31 21:50:13 -0700

    added form validators

--- src/recipes/pages/NewRecipe.js
+++ src/recipes/pages/NewRecipe.js
@@ -1,12 +1,20 @@
 import React from "react";
 import Input from "../../shared/components/FormElements/Input";
+import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
 
 import "./NewRecipe.css";
 
 const NewRecipe = () => {
   return (
     <form className="recipe-form">
-      <Input element="input" type="text" label="Title" />
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[]}
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title."
+      />
     </form>
   );
 };
