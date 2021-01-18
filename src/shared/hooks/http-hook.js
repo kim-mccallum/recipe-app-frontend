194fdb50e3209e209048d9e4f89354413a67c2cb
Kim McCallum <kchaase@gmail.com>
2021-01-18 12:49:56 -0700

    added custom http-hook and user request logic

---
+++ src/shared/hooks/http-hook.js
@@ -0,0 +1,54 @@
+import { useState, useCallback, useRef, useEffect } from "react";
+
+export const useHttpClient = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
+  const activeHttpRequests = useRef([]); //store data across rerender cycles
+
+  const sendRequest = useCallback(
+    async (url, method = "GET", body = null, headers = {}) => {
+      setIsLoading(true);
+      const httpAbortCtrl = new AbortController(); //built into modern browsers
+      activeHttpRequests.current.push(httpAbortCtrl); //make sure you don't error in case someone changes page before request is done
+
+      try {
+        const response = await fetch(url, {
+          method,
+          body,
+          headers,
+          signal: httpAbortCtrl.signal,
+        });
+
+        const responseData = await response.json();
+        //clear the abort controls from the reponse that just completed
+        activeHttpRequests.current = activeHttpRequests.current.filter(
+          (reqCtrl) => reqCtrl !== httpAbortCtrl
+        );
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        setIsLoading(false);
+        return responseData;
+      } catch (err) {
+        setError(err.message);
+        setIsLoading(false);
+        throw err;
+      }
+    },
+    []
+  );
+
+  const clearError = () => {
+    setError(null);
+  };
+
+  useEffect(() => {
+    return () => {
+      activeHttpRequests.current.forEach((abortCtrl) => abortCtrl.abort());
+    }; //cleanup logic - never continue with a cancelled request
+  }, []);
+  return { isLoading, error, sendRequest, clearError };
+};
