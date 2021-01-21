9818203d21df4111bddd4ae916cbf3e59029b940
Kim McCallum <kchaase@gmail.com>
2021-01-21 09:16:43 -0700

    added update recipe methods

--- src/recipes/pages/UpdateRecipe.js
+++ src/recipes/pages/UpdateRecipe.js
@@ -1,49 +1,28 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState, useContext } from "react";
+import { useParams, useHistory } from "react-router-dom";
 
 import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
 import Card from "../../shared/components/UIElements/Card";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
 import {
   VALIDATOR_REQUIRE,
   VALIDATOR_MINLENGTH,
 } from "../../shared/util/validators";
 import { useForm } from "../../shared/hooks/form-hook";
+import { useHttpClient } from "../../shared/hooks/http-hook";
+import { AuthContext } from "../../shared/context/auth-context";
 
 import "./RecipeForm.css";
 
-const DUMMY_RECIPES = [
-  {
-    id: "r1",
-    title: "Apple with almond butter",
-    description: "Perfect, filling snack on the go!",
-    imageUrl:
-      "https://www.chelanfresh.com/wp-content/uploads/2019/08/Lucy-Glo_NEW1.png",
-    ingredients:
-      "Apple (pink lady, honey crisp or Lucy Glo), 2 Tablespoons of almond butter",
-    instructions:
-      "Slice the apple (optional), smear almond butter on it and eat it!",
-    creator: "u1",
-  },
-  {
-    id: "r2",
-    title: "Avocado Toast",
-    description:
-      "Quick filling snack with whole grain bread and rich, delicious avocado!",
-    imageUrl:
-      "https://fsi.colostate.edu/wp-content/uploads/2016/02/Coveravocado.jpg",
-    ingredients:
-      "2 slices of sprouted whole grain bread, 0.5 ripe avocado, 1 tablespoon nutritional yeast.",
-    instructions:
-      "Toast the bread. Slice the avocado in the skin, scoop it out with a fork and smash it onto the bread. Sprinkle with nutritional yeast and serve.",
-    creator: "u2",
-  },
-];
-
 const UpdateRecipe = () => {
-  const [isLoading, setIsLoading] = useState(true);
+  const auth = useContext(AuthContext);
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [loadedRecipe, setLoadedRecipe] = useState();
   //extract from the URL that is part of the URL set up in the routes
   const recipeId = useParams().recipeId;
+  const history = useHistory();
 
   const [formState, inputHandler, setFormData] = useForm(
     {
@@ -68,42 +47,72 @@ const UpdateRecipe = () => {
     true
   );
 
-  const identifiedRecipe = DUMMY_RECIPES.find((r) => r.id === recipeId);
-
   //avoid infinite loop by wrapping call in useEffect
   useEffect(() => {
-    if (identifiedRecipe) {
-      setFormData(
-        {
-          title: {
-            value: identifiedRecipe.title,
-            isValid: true,
-          },
-          description: {
-            value: identifiedRecipe.description,
-            isValid: true,
-          },
-          ingredients: {
-            value: identifiedRecipe.ingredients,
-            isValid: true,
-          },
-          instructions: {
-            value: identifiedRecipe.instructions,
-            isValid: true,
-          },
-        },
-        true
-      );
-    }
-    setIsLoading(false);
-  }, [setFormData, identifiedRecipe]);
+    const fetchRecipe = async () => {
+      try {
+        const responseData = await sendRequest(
+          `http://localhost:5000/api/recipes/${recipeId}`
+        );
 
-  const recipeUpdateSubmitHandler = (event) => {
+        setLoadedRecipe(responseData.recipe);
+        setFormData(
+          {
+            title: {
+              value: responseData.recipe.title,
+              isValid: true,
+            },
+            description: {
+              value: responseData.recipe.description,
+              isValid: true,
+            },
+            ingredients: {
+              value: responseData.recipe.ingredients,
+              isValid: true,
+            },
+            instructions: {
+              value: responseData.recipe.instructions,
+              isValid: true,
+            },
+          },
+          true
+        );
+      } catch (err) {}
+    };
+    fetchRecipe();
+  }, [sendRequest, recipeId, setFormData]);
+
+  const recipeUpdateSubmitHandler = async (event) => {
     event.preventDefault();
-    console.log(formState.inputs);
+    //send post request to update
+    try {
+      await sendRequest(
+        `http://localhost:5000/api/recipes/${recipeId}`,
+        "PATCH",
+        JSON.stringify({
+          title: formState.inputs.title.value,
+          description: formState.inputs.description.value,
+          ingredients: formState.inputs.ingredients.value,
+          instructions: formState.inputs.instructions.value,
+        }),
+        {
+          "Content-Type": "application/json",
+        }
+      );
+      //just redirect to overview of places
+      history.push(`/${auth.userId}/recipes`);
+    } catch (err) {}
   };
 
-  if (!identifiedRecipe) {
+  //temporary workaround - only render if you have values
+  if (isLoading) {
+    return (
+      <div className="center">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+  if (!loadedRecipe && !error) {
     return (
       <div className="center">
         <Card>
@@ -112,60 +121,57 @@ const UpdateRecipe = () => {
       </div>
     );
   }
-  //temporary workaround - only render if you have values
-  if (isLoading) {
-    return (
-      <div className="center">
-        <h2>Loading...</h2>
-      </div>
-    );
-  }
   return (
-    <form className="recipe-form" onSubmit={recipeUpdateSubmitHandler}>
-      <Input
-        id="title"
-        element="input"
-        label="Title"
-        validators={[VALIDATOR_REQUIRE()]}
-        errorText="Please enter a valid title."
-        onInput={inputHandler}
-        initialValue={formState.inputs.title.value}
-        initialValid={formState.inputs.title.isValid}
-      />
-      <Input
-        id="description"
-        element="textarea"
-        label="Description"
-        validators={[VALIDATOR_MINLENGTH(5)]}
-        errorText="Please enter a valid description (at least 5 characters)."
-        onInput={inputHandler}
-        initialValue={formState.inputs.description.value}
-        initialValid={formState.inputs.description.isValid}
-      />
-      <Input
-        id="inredients"
-        element="textarea"
-        label="Ingredients"
-        validators={[VALIDATOR_MINLENGTH(5)]}
-        errorText="Please enter a valid set of inredients (at least 5 characters)."
-        onInput={inputHandler}
-        initialValue={formState.inputs.ingredients.value}
-        initialValid={formState.inputs.ingredients.isValid}
-      />
-      <Input
-        id="instructions"
-        element="textarea"
-        label="Instructions"
-        validators={[VALIDATOR_MINLENGTH(5)]}
-        errorText="Please enter a valid set of instructions (at least 5 characters)."
-        onInput={inputHandler}
-        initialValue={formState.inputs.instructions.value}
-        initialValid={formState.inputs.instructions.isValid}
-      />
-      <Button type="submit" disabled={!formState.isValid}>
-        UPDATE RECIPE
-      </Button>
-    </form>
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      {!isLoading && loadedRecipe && (
+        <form className="recipe-form" onSubmit={recipeUpdateSubmitHandler}>
+          <Input
+            id="title"
+            element="input"
+            label="Title"
+            validators={[VALIDATOR_REQUIRE()]}
+            errorText="Please enter a valid title."
+            onInput={inputHandler}
+            initialValue={loadedRecipe.title}
+            initialValid={true}
+          />
+          <Input
+            id="description"
+            element="textarea"
+            label="Description"
+            validators={[VALIDATOR_MINLENGTH(5)]}
+            errorText="Please enter a valid description (at least 5 characters)."
+            onInput={inputHandler}
+            initialValue={loadedRecipe.description}
+            initialValid={true}
+          />
+          <Input
+            id="inredients"
+            element="textarea"
+            label="Ingredients"
+            validators={[VALIDATOR_MINLENGTH(5)]}
+            errorText="Please enter a valid set of inredients (at least 5 characters)."
+            onInput={inputHandler}
+            initialValue={loadedRecipe.ingredients}
+            initialValid={true}
+          />
+          <Input
+            id="instructions"
+            element="textarea"
+            label="Instructions"
+            validators={[VALIDATOR_MINLENGTH(5)]}
+            errorText="Please enter a valid set of instructions (at least 5 characters)."
+            onInput={inputHandler}
+            initialValue={loadedRecipe.instructions}
+            initialValid={formState.inputs.instructions.isValid}
+          />
+          <Button type="submit" disabled={!formState.isValid}>
+            UPDATE RECIPE
+          </Button>
+        </form>
+      )}
+    </React.Fragment>
   );
 };
 
