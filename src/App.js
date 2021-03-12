cb5b56c03ba74691658198d69ada55148b481ec9
Kim McCallum <kchaase@gmail.com>
2021-03-11 17:47:49 -0700

    added timer to logout and clear

--- src/App.js
+++ src/App.js
@@ -14,15 +14,19 @@ import UpdateRecipe from "./recipes/pages/UpdateRecipe";
 import Auth from "./user/pages/Auth";
 import { AuthContext } from "./shared/context/auth-context";
 
+let logoutTimer;
+
 const App = () => {
   const [token, setToken] = useState(false);
+  const [tokenExpirationDate, setTokenExpirationDate] = useState();
   const [userId, setUserId] = useState(false);
+
   //wrap with useCallback so that the function is not recreated unnecessarily
   const login = useCallback((uid, token, expirationDate) => {
     setToken(token);
     setUserId(uid);
     const tokenExpirationDate =
-      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60); //current time plus 1 hour
+      expirationDate || new Date(new Date().getTime() + 2000); //current time plus 1 hour
     localStorage.setItem(
       "userData",
       JSON.stringify({
@@ -35,9 +39,20 @@ const App = () => {
 
   const logout = useCallback(() => {
     setToken(null);
+    setTokenExpirationDate(null);
     setUserId(null);
     localStorage.removeItem("userData");
   }, []);
+
+  useEffect(() => {
+    if (token && tokenExpirationDate) {
+      const remainingTime =
+        tokenExpirationDate.getTime() - new Date().getTime();
+      logoutTimer = setTimeout(logout, remainingTime); //set new timer
+    } else {
+      clearTimeout(logoutTimer); //clear ongoing timers
+    }
+  }, [token, logout, tokenExpirationDate]);
 
   useEffect(() => {
     const storedData = JSON.parse(localStorage.getItem("userData"));
